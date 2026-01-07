1b584a09e950740ec1467e1160c338786af2d273
Josuedavid27 <senajosuedavidtorres@gmail.com>
2026-01-06 21:58:19 -0500

    variables con javascript

---
+++ JavaScript/variables.js
@@ -0,0 +1,7 @@
+let edad = 18; // Declaración de una variable llamada 'edad' y asignación del valor 25
+const nombre = "Josue"; // Declaración de una variable llamada 'nombre' y asignación del valor "Josue"
+var esEstudiante = true; // Declaración de una variable llamada 'esEstudiante' y asignación del valor true
+
+console.log("Edad:", edad); // Imprime: Edad: 18
+console.log("Nombre:", nombre); // Imprime: Nombre: Josue   
+console.log("¿Es estudiante?", esEstudiante); // Imprime: ¿Es estudiante? true
